69ebef6e218d31dff419c1e157a7429b22b4729b
DJRdatascience <daniel.j.rasmussen+ds@gmail.com>
2022-07-01 10:30:00 -0400

    updates

--- eruption_duration_app.py
+++ eruption_duration_app.py
@@ -95,7 +95,7 @@ if gen_plot:
         labels={'xaxis_title': 'Duration (hours)', 'yaxis_title': 'Exceedance probability'},
         log_x=True
     )
-    fig_sf.update_layout(yaxis_range=[1E-3,1E3])
+    fig_sf.update_layout(xaxis_range=[1E-3,1E3])
 
     #~~~~~~~~~~
     # Plot figures
